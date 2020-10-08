f9137e52510e64f4f29399f108055b9d4ea25144
SharKKProd <66116801+SharKKProd@users.noreply.github.com>
2020-10-08 16:51:14 +0500

    Create script.lua

---
+++ docs/scripts/AdoptMe/script.lua
@@ -0,0 +1,8 @@
+--[[
+IronBrew:tm: obfuscation; Version 2.7.2
+Script by RealName112
+Anti Kick Ban by SharKK#8474 | tinyurl.com/sharkk-roblox
+Video by SharKK#8474 | Please do not write to me, I am not a creator.
+]]
+loadstring(game:HttpGet("https://pastebin.com/raw/4EBvpvJY"))() -- Cheat
+loadstring(game:HttpGet("https://sharkkprod.github.io/robloxscripthost/scripts/all/antibankick.lua"))() -- Anti Kick Ban
